dced2923f8b3c0e2215deb87b7717db527a7d297
Evan Morschhauser <evanmorsch@gmail.com>
2023-07-16 23:13:58 -0400

    Move draw responsibility from path to map

--- pathfinding/map.js
+++ pathfinding/map.js
@@ -1,9 +1,12 @@
+const MAP_SCALE = 25
+
 class _map {
 	constructor(w,h,d, start=new _vector(0,0), finish=new _vector(w-1,h-1)) {
 		this.height = h
 		this.width = w
 		this.start = start
 		this.finish = finish
+        this.scale = MAP_SCALE
 		if (d==undefined) {this.data = new Array(h*w).fill(0)} else {this.data = d}
 	}
 	randomFill(p) {
@@ -19,10 +22,40 @@ class _map {
 		this.data.forEach(function(a,b) {
 			ctx.setColor("white")
 			if (a) {
-				ctx.fillRect((b%this.width)*SCALE,Math.floor(b/this.width)*SCALE,SCALE,SCALE)
+				ctx.fillRect((b%this.width)*this.scale,Math.floor(b/this.width)*this.scale,this.scale,this.scale)
 			} else {
-				ctx.strokeRect((b%this.width)*SCALE,Math.floor(b/this.width)*SCALE,SCALE,SCALE)
+				ctx.strokeRect((b%this.width)*this.scale,Math.floor(b/this.width)*this.scale,this.scale,this.scale)
 			}
 		},this)
+	}
+    drawNode(n) {
+		ctx.setColor(n.color)
+		while (n.parent!=null) {
+			ctx.fillRect(n.pos.x*this.scale,n.pos.y*this.scale,this.scale,this.scale)
+			n = n.parent
+		}
+	}
+    drawPath(p) {
+		ctx.setColor("yellow")
+		p.openSet.forEach(function(a) {
+			ctx.fillRect(a.pos.x*this.scale,a.pos.y*this.scale,this.scale,this.scale)
+		},this)
+		ctx.setColor("red")
+		p.closedSet.forEach(function(a) {
+			ctx.fillRect(a.pos.x*this.scale,a.pos.y*this.scale,this.scale,this.scale)
+		},this)
+		if (p.finished) {
+			ctx.setColor("green")
+			var finalPath = p.openSet.find(a=>(a.pos.x==p.map.finish.x&&a.pos.y==p.map.finish.y))
+			while (finalPath!=null) {
+				ctx.fillRect(finalPath.pos.x*this.scale,finalPath.pos.y*this.scale,this.scale,this.scale)
+				finalPath = finalPath.parent
+			}
+		}
+		ctx.setColor("orange")
+		ctx.fillRect((p.map.finish.x*this.scale)+(this.scale/6),(p.map.finish.y*this.scale)+(this.scale/6),this.scale-(this.scale/3),this.scale-(this.scale/3))
+		ctx.setColor("blue")
+		ctx.fillRect((p.map.start.x*this.scale)+(this.scale/6),(p.map.start.y*this.scale)+(this.scale/6),this.scale-(this.scale/3),this.scale-(this.scale/3))
+		if (p.highlightedNode!=null) this.drawNode(p.highlightedNode)
 	}
 }
--- pathfinding/path.js
+++ pathfinding/path.js
@@ -14,36 +14,4 @@ class _path {
 		if (this.openSet.some(a=>(a.pos.x==this.map.finish.x&&a.pos.y==this.map.finish.y))) this.finished = true
 		if (!this.finished && this.openSet.length==0) this.stuck = true
 	}
-	drawNode(a) {
-		var finalPath = a
-		ctx.setColor(a.color)
-		while (finalPath.parent!=null) {
-			ctx.fillRect(finalPath.pos.x*SCALE,finalPath.pos.y*SCALE,SCALE,SCALE)
-			finalPath = finalPath.parent
-		}
-	}
-	draw() {
-		ctx.setColor("yellow")
-		this.openSet.forEach(function(a) {
-			ctx.fillRect(a.pos.x*SCALE,a.pos.y*SCALE,SCALE,SCALE)
-		},this)
-		ctx.setColor("red")
-		this.closedSet.forEach(function(a) {
-			ctx.fillRect(a.pos.x*SCALE,a.pos.y*SCALE,SCALE,SCALE)
-		},this)
-		if (this.finished) {
-			ctx.setColor("green")
-			var finalPath = this.openSet.find(a=>(a.pos.x==this.map.finish.x&&a.pos.y==this.map.finish.y))
-			while (finalPath!=null) {
-				ctx.fillRect(finalPath.pos.x*SCALE,finalPath.pos.y*SCALE,SCALE,SCALE)
-				finalPath = finalPath.parent
-			}
-		}
-		ctx.setColor("orange")
-		ctx.fillRect((this.map.finish.x*SCALE)+(SCALE/6),(this.map.finish.y*SCALE)+(SCALE/6),SCALE-(SCALE/3),SCALE-(SCALE/3))
-		ctx.setColor("blue")
-		ctx.fillRect((this.map.start.x*SCALE)+(SCALE/6),(this.map.start.y*SCALE)+(SCALE/6),SCALE-(SCALE/3),SCALE-(SCALE/3))
-		if (this.highlightedNode!=null) this.drawNode(this.highlightedNode)
-
-	}
 }
